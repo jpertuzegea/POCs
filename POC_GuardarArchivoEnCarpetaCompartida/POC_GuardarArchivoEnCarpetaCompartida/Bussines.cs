ca9f56775d65712a8623946d3c0deb9a1f3d006b
jpertuzegea <jpertuzegea@hotmail.com>
2020-05-28 16:26:05 -0500

    TASK_#001 - POC Guardar Archivo en carpeta compartida por red

---
+++ POC_GuardarArchivoEnCarpetaCompartida/POC_GuardarArchivoEnCarpetaCompartida/Bussines.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace POC_GuardarArchivoEnCarpetaCompartida
+{
+    public class Bussines
+    {
+        public static object ObjSave = new object();
+
+        // Metodo para guardar archivo en carpeta comnpartida 
+        public bool SaveFileInFoldershared()
+        {
+            string FileBase64 = @"U2Vydmlkb3IgQkQgRGVzYXJyb2xsbyAtLT4gMTAuMTAwLjEwMi4zNw0KVXN1YXJpbyAtLT4gZGF0YWJhc2VzDQpDbGF2ZSAtLT4gZGF0YWJhc2VzDQoNCg0KUG9ydGFsDQpodHRwOi8vMTAuMTAwLjEwMi4xMzE6ODA4My9Db2xzdWJzaWRpby9EZWZhdWx0LmFzcHg/dGFiaWQ9MzYNClVzdWFyaW86IGRubmhvc3QNCkNsYXZlOiBjb2xvbWJpYQ0KDQoNCmh0dHBzOi8vZXZlcnRlY2luY0BkZXYuYXp1cmUuY29tL2V2ZXJ0ZWNpbmMvUGljYXNzby9fZ2l0L1BpY2Fzc28=";
+            string FileName = "JorgePertuzEgea.txt";
+
+            try
+            {
+                lock (ObjSave)
+                {
+                    string targetPath = @"//192.168.99.135/HeadsCompartida/JoRgUeMunita/";
+                    byte[] FileByteArray = Convert.FromBase64String(FileBase64);
+                    File.WriteAllBytes(targetPath + FileName, FileByteArray);
+                    return true;
+                }
+            }
+            catch (Exception Error)
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ POC_GuardarArchivoEnCarpetaCompartida/POC_GuardarArchivoEnCarpetaCompartida/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace POC_GuardarArchivoEnCarpetaCompartida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prueba Guardado de Archivo en Base64 en SFTP");
+
+            Bussines Bussines = new Bussines();
+
+            if (Bussines.SaveFileInFoldershared())
+            {
+                Console.WriteLine("\n Archivo Guardado Con Exito");
+            }
+            else
+            {
+                Console.WriteLine("\n Archivo NO Guardado");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
