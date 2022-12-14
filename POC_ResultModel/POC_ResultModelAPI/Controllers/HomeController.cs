1b16ee2472697a0e577d3f97745d75c2d276e595
jpertuzegea <jpertuzegea@hotmail.com>
2022-12-13 22:23:27 -0500

    Implementacion ResultMModel<T>

---
+++ POC_ResultModel/POC_ResultModelAPI/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POC_ResultModelAPI.Models;
+
+namespace POC_ResultModelAPI.Controllers
+{
+    [Route("api/Prueba")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+        [HttpGet("Index")]
+        public async Task<ActionResult<ResultModel<PersonModel>>> Index()
+        {
+            PersonModel PersonModel = new PersonModel();
+            PersonModel.Id = 1;
+            PersonModel.FullName = "Jorge Pertuz Egea";
+            PersonModel.Email = "jpertuzegea@hotmail.com";
+
+            ResultModel<PersonModel> response = new ResultModel<PersonModel>()
+            {
+                IsSuccess = true,
+                Messages = "",
+                Result = PersonModel
+            };
+
+            return response;
+        }
+    }
+}
---
+++ POC_ResultModel/POC_ResultModelAPI/Models/PersonModel.cs
@@ -0,0 +1,9 @@
+﻿namespace POC_ResultModelAPI.Models
+{
+    public class PersonModel
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+    }
+}
